00e1256a2c19d306885de6a865e54f75a818161d
Dexter <raultasayco54321@gmail.com>
2025-06-17 06:30:46 -0500

    inserciones

---
+++ src/auth/views/sign-up-view/sign-up-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-view',
+  imports: [],
+  templateUrl: './sign-up-view.component.html',
+  styleUrl: './sign-up-view.component.css'
+})
+export class SignUpViewComponent {
+
+}
