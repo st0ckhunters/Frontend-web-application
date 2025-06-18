affc4bd82da7c27604db3619f3cbd95d2901c7b6
Dexter <raultasayco54321@gmail.com>
2025-06-18 17:54:29 -0500

    arreglo

--- src/auth/views/sign-up-view/sign-up-view.component.ts
+++ src/auth/views/sign-up-view/sign-up-view.component.ts
@@ -1,29 +1,36 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sign-up-view',
+  standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslatePipe
   ],
   templateUrl: './sign-up-view.component.html',
   styleUrl: './sign-up-view.component.css'
 })
 export class SignUpViewComponent {
-  loginForm: FormGroup;
+  registerForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
   }
 
   onSubmit(): void {
-    if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-      console.log('Login:', email, password);
-      // Aquí puedes llamar a tu servicio de autenticación
+    if (this.registerForm.valid) {
+      const { name, lastName, email, password } = this.registerForm.value;
+      console.log('Registro:', { name, lastName, email, password });
+      // Aquí puedes llamar al servicio de registro
+    } else {
+      console.warn('Formulario inválido');
     }
   }
 }
