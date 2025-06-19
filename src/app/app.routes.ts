8656a4856d8fde2313b546aca742e3e4bd6fda4f
Dexter <raultasayco54321@gmail.com>
2025-06-19 01:12:20 -0500

    Adicion account frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import {LogInViewComponent} from '../auth/views/log-in-view/log-in-view.componen
 import {SignUpViewComponent} from '../auth/views/sign-up-view/sign-up-view.component';
 import {NotFoundError} from 'rxjs';
 import {NotFoundComponent} from '../shared/components/notfound/not-found/not-found.component';
+import {ItemUsersComponent} from '../auth/components/item-users/item-users.component';
+import {AccountViewComponent} from '../auth/views/account-view/account-view.component';
 
 export const routes: Routes = [
   { path: '', component: LogInViewComponent },
   { path: 'login', component: LogInViewComponent },
   { path: 'signup', component: SignUpViewComponent },
+  { path: 'item-users', component: ItemUsersComponent },
+  { path: 'accounts', component: AccountViewComponent},
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: 'not-found' }
 ];
