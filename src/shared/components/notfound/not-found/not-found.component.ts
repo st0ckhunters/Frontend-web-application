e2fdc2f32156fde4bd71bb7fd5f2d0b97ff8f04a
Dexter <raultasayco54321@gmail.com>
2025-06-18 20:33:49 -0500

    implementación de routeo con el header

---
+++ src/shared/components/notfound/not-found/not-found.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [
+    MatButton,
+  ],
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.css'
+})
+export class NotFoundComponent {
+
+}
